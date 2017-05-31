65c0928e007cc6759fbab99e4d3502d32988e509
netstorm <netstorm@info@cavisson.com>
2017-05-31 17:54:11 +0530

    new version

--- checkDependency.js
+++ checkDependency.js
@@ -5,14 +5,14 @@
 var npm = require('npm');
 var util = require('./lib/utils/compare-verssions.js')
 var nodeVersion = process.version
-var version = util(nodeVersion,'v1.0.0')
+var version = util(nodeVersion,'v4.0.0')
 npm.load(function(err) {
 
   // install module ffi
-if(version == 1)
-  npm.commands.install(['falafel@2.1.0'], function(er, data) {});
+if(version == 1 || version == 0)
+  npm.commands.install(['falafel@2.1.0'], function(er, data) {if(er)console.log("Fail to install falafel@2.1.0")});
 else if( version == -1)
-  npm.commands.install(['falafel@0.3.1'], function(er, data) {});
+  npm.commands.install(['falafel@0.3.1'], function(er, data) {if(er)console.log("Fail to install falafel@0.3.1")});
 
   npm.on('log', function(message) {
     // log installation progress
