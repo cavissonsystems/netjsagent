b969290a777c32167bf5e7efaa29b9d0c0f27132
netstorm <netstorm@info@cavisson.com>
2017-06-07 20:48:34 +0530

    bt bugs

--- checkDependency.js
+++ checkDependency.js
@@ -3,20 +3,20 @@
 * This file will check version of nodeJs , and on basis of this version, falafel version will be decided
 * */
 try{
-var npm = require('npm');
-var util = require('./lib/utils/compare-verssions.js')
-var nodeVersion = process.version
-var version = util(nodeVersion,'v4.0.0')
-npm.load(function(err) {
+  var npm;
+  var util = require('./lib/utils/compare-verssions.js')
+  var nodeVersion = process.version
+  var version = util(nodeVersion,'v4.0.0');
 
-  // install module ffi
-if(version == 1 || version == 0)
-  npm.commands.install(['falafel@2.1.0'], function(er, data) {if(er)console.log("Fail to install falafel@2.1.0")});
-else if( version == -1)
-  npm.commands.install(['falafel@0.3.1'], function(er, data) {if(er)console.log("Fail to install falafel@0.3.1")});
+  if( version == -1){
+    npm = require('npm');
+    npm.load(function(err) {
+      // install module falafel
+      npm.commands.install(['falafel@0.3.1'], function(er, data) {if(er)console.log("Fail to install falafel@0.3.1")});
+      npm.on('log', function(message) {});
+    });
+  }
+  else
+    return;
 
-  npm.on('log', function(message) {
-    // log installation progress
-  });
-});
 }catch(err){console.log("Cant find module npm ")}
