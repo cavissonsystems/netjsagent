3fc1ff76a5eb8b4b0e49cf9fab570fdebe5249aa
sahildhall <sahildhall012@gmail.com>
2017-11-16 18:21:28 +0530

    bug fix

--- netjsagent.js
+++ netjsagent.js
@@ -62,7 +62,7 @@ NJSInstrument.prototype.instrument = function instrument(args)
         }
 	if(agentSetting.agentMode >= 3)	{
 		njstrace.inject(null,instPrfParseobj.getInstrMap(),agentSetting.enableWraping);                    //injecting our code into applications code
-        require('./lib/utils/instrumentNodeEvents')(agentSetting.corelateEventCallback)
+        require('./lib/utils/instrumentNodeEvents')(agentSetting.corelateEventCallback, agentSetting.wrapAsyncApi)
 	}
 	if(agentSetting.agentMode >= 2){
 		require('./lib/nodetime/index').profile();
